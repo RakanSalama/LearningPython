6ba5be7fcb1fa5d4bceaafbb2b86a892f86222bc
RakanSalama <superrakan@hotmail.com>
2022-12-23 02:37:00 +0300

    Day6

---
+++ LEC20_Iterators/Part01_Iterators.py
@@ -0,0 +1,62 @@
+#An iterator is an object that contains a countable number of values.
+#Lists, tuples, dictionaries, and sets are all iterable objects.
+#All these objects have a iter() method which is used to get an iterator:
+
+#In a list
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+#In a String
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print("-----------------")
+######################
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+##################
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
