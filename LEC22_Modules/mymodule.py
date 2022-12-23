9328d62fb3f9b90b58a00087e4ad7f859e6040fc
RakanSalama <superrakan@hotmail.com>
2022-12-24 02:59:11 +0300

    Day7

---
+++ LEC22_Modules/mymodule.py
@@ -0,0 +1,5 @@
+def greeting(name):
+  print("Hello, " + name)
+  # Here you add the function and in part01 you can call it by 
+  # using mymodule.(name of the function like greeting) and it will 
+  # do the work :)
