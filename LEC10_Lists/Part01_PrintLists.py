216bb7123a979fa79c7ba1f9d26536d34370e5c2
RakanSalama <superrakan@hotmail.com>
2022-12-24 15:07:55 +0300

    Day7

--- LEC10_Lists/Part01_PrintLists.py
+++ LEC10_Lists/Part01_PrintLists.py
@@ -19,13 +19,15 @@ as we know, we use x = [1,2,3,4] OR x = list((1,2,3,4))
 x = list(("Rakan","Rayan","Remah"))
 print(x)
 """
-
+numbers = [1,3,3,4]
+print(numbers)
 """
 List -> is a collection which is ordered and changeable. Allows duplicate members.                               Duplicated = 1,1,2,2,3,3 etc.. 
 Tuple-> is a collection which is ordered and unchangeable. Allows duplicate members.
 Set  -> is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
 Dictionary -> is a collection which is ordered** and changeable. No duplicate members.
 """
+
 
 
 
---
+++ LEC24_Math/Part01_Math.py
@@ -0,0 +1,40 @@
+#min= smallest value , max= biggest value
+x = min(5,10,25)
+y = max(5,10,25)
+print("x=",x,"y=",y)
+
+#Abs= to get a positive value of the inserted number:
+x = abs(-4.20)
+print(x)
+
+#To get the power of a number for example 3^6
+x = pow(3,6) # 3*3*3*3*3*3
+print(x)
+
+#If you want to implement more math function you can implement Math module
+#import math
+#Here are some functions that the module will allow you to 
+#do if you implement it:
+import math
+
+#math.sqrt()
+x = math.sqrt(49)
+print(x)
+
+#math.ceil / math.floor
+x = math.ceil(1.9)
+print("ceil is:",x) # Returns 2
+x = math.floor(1.9)
+print("floor is:",x) # Returns 1
+
+#Math.pi
+x = math.pi
+print(x)
+
+
+
+
+
+
+
+
